2cc1df5ac924567637543176302a4e32b89c7eec
unknown <alex.m.sky@gmail.com>
2018-03-18 19:15:37 +0300

    переключатели сделаны через target

--- app/resources/scripts/js.js
+++ app/resources/scripts/js.js
@@ -1,3 +1,16 @@
-// function hoverDisable() {
-//   this.style.color = 'red';
-// }
+var items = document.getElementById('items');
+
+items.onclick = function(event) {
+  var target = event.target;
+  while (target != this) {
+    if (target.className == 'item' || target.className == 'item selected') {
+      highlight(target);
+      return;
+    }
+    target = target.parentNode;
+  }
+}
+
+function highlight(node) {
+  node.classList.toggle('selected');
+}
