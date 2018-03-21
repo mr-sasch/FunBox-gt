63441c523f4c451898c9f8a56293f9149400278a
unknown <alex.m.sky@gmail.com>
2018-03-21 19:04:49 +0300

    wednsday

--- app/resources/scripts/js.js
+++ app/resources/scripts/js.js
@@ -1,30 +1,5 @@
 var items = document.getElementById('items');
 
-items.onclick = function(event) {
-
-  var target = event.target;
-  while (target != this) {
-    if (target.className == 'item' || target.className == 'item selected') {
-      highlight(target);
-      return;
-    }
-    target = target.parentNode;
-  }
-
-}
-
-function highlight(node) {
-  var elem = node.classList;
-  elem.toggle('selected');
-}
-
-
-// ховер
-items.onmouseenter = function() {
-  document.body.style.pointerEvents = 'none';
-  return;
-}
-items.onmouseleave = function() {
-  document.body.style.pointerEvents = 'initial';
-  return;
+items.onclick = function() {
+  this.classList.toggle('selected');
 }
