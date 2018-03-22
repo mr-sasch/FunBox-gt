bc81e05f3d7fa8df2efdadb4fec626fefb8bd15e
unknown <alex.m.sky@gmail.com>
2018-03-22 15:10:00 +0300

    'finished'

--- app/resources/scripts/js.js
+++ app/resources/scripts/js.js
@@ -1,5 +1,30 @@
 var items = document.getElementById('items');
+var currentElem;
 
-items.onclick = function() {
-  this.classList.toggle('selected');
+items.onclick = function(event) {
+
+  var target = event.target;
+  while(target!=this) {
+    if(target.classList.contains('ended')) return;
+    if(target.classList.contains('item')) break;
+    target = target.parentNode;
+  }
+  if(target == this) return;
+
+  target.classList.add('not-hover');
+  target.classList.toggle('selected');
+  currentElem = target;
+}
+
+items.onmouseout = function(event) {
+  if(!currentElem) return;
+
+  var relatedTarget = event.relatedTarget;
+  if(relatedTarget) {
+    while (relatedTarget) {
+      if(relatedTarget == currentElem) return;
+      relatedTarget = relatedTarget.parentNode;
+    }
+  }
+  currentElem.classList.remove('not-hover');
 }
